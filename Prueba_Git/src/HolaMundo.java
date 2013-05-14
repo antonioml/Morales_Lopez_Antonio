d9d395dbe060570f53f8f0c07bbed2ca3c0fd41d
usuario2antonio <amoralesdesign@gmail.com>
2013-05-14 21:34:03 +0200

    Commit de usuario2antonio, prueba.

--- Prueba_Git/src/HolaMundo.java
+++ Prueba_Git/src/HolaMundo.java
@@ -1,6 +1,6 @@
 
 public class HolaMundo {
 public static void main(String args[]){
-	System.out.println("Hola Mundo");
+	System.out.println("Hola estoy en el anfitrión de usuario2antonio modificando el repositorio de antonioml");
 }
 }
